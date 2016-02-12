e3438321f3bf314f9e949ba2f04da3f3c7ee375e
Srdjan Pajic <pajics@gmail.com>
2016-02-12 22:28:50 +0100

    added logging to home controller (still missing 3rd party provider - nlog, serilog, elmah.io, ...)

--- App/src/Market/Controllers/HomeController.cs
+++ App/src/Market/Controllers/HomeController.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Market.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(
+        ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
+            _logger.LogDebug("Index");
             return View();
         }
 
