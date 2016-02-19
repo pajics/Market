8d400f985e678af510def77e1f39c46a52dc0242
Srdjan Pajic <s.pajic@diamonddogs.cc>
2016-02-19 16:46:07 +0100

    adding a view component (child action upgrade)

---
+++ App/src/Market/Components/FeaturedProductsViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Market.ViewModels.Components;
+using Microsoft.AspNet.Mvc;
+
+namespace Market.Components
+{
+    public class FeaturedProductsViewComponent  : ViewComponent
+    {
+        public FeaturedProductsViewComponent ()
+        {
+            
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(new FeaturedProductViewModel());
+        }
+    }
+}
---
+++ App/src/Market/ViewModels/Components/FeaturedProductViewModel.cs
@@ -0,0 +1,7 @@
+namespace Market.ViewModels.Components
+{
+    public class FeaturedProductViewModel
+    {
+
+    }
+}
