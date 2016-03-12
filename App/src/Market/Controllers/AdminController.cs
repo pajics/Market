fed5f9b62ff7dd295d525d85c9388ab15b92a817
Srdjan Pajic <pajics@gmail.com>
2016-03-13 00:51:59 +0100

    adding admin layout
    https://github.com/almasaeed2010/AdminLTE

---
+++ App/src/Market/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Authorization;
+using Microsoft.AspNet.Mvc;
+
+namespace Market.Controllers
+{
+    //[Authorize(Roles = "Admin")]
+    public class AdminController : BaseController
+    {
+        public IActionResult Dashboard()
+        {
+            var viewModel = new DashboardViewModel();
+            return View("Dashboard", viewModel);
+        }
+    }
+
+    public class DashboardViewModel
+    {
+    }
+}
